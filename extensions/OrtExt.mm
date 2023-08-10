408508d6a46808b9b62e6fc1f17e2b7535dbca8a
rachguo <rachguo@rachguos-Mac-mini.local>
2023-08-10 15:26:37 -0700

    move to the header for function doc

--- extensions/OrtExt.mm
+++ extensions/OrtExt.mm
@@ -8,11 +8,6 @@
 @implementation OrtExt
 
 + (nonnull ORTCAPIRegisterCustomOpsFnPtr)getRegisterCustomOpsFunctionPointer {
-
-  // Note: This returns the address of `RegisterCustomOps` function. At swift
-  // level, user can call this function to get the RegisterCustomOpsFnPtr
-  // and use the function pointer to register custom ops. See
-  // SwiftOnnxRuntimeExtensionsTests.swift for an example usage.
   return RegisterCustomOps;
 }
 
--- extensions/include/OrtExt.h
+++ extensions/include/OrtExt.h
@@ -8,6 +8,10 @@
 typedef struct OrtStatus* (*ORTCAPIRegisterCustomOpsFnPtr)(struct OrtSessionOptions* /*options*/,
                                                            const struct OrtApiBase* /*api*/);
 
+// Note: This returns the address of `RegisterCustomOps` function. At swift
+// level, user can call this function to get the RegisterCustomOpsFnPtr
+// and use the function pointer to register custom ops. See
+// SwiftOnnxRuntimeExtensionsTests.swift for an example usage.
 + (nonnull ORTCAPIRegisterCustomOpsFnPtr)getRegisterCustomOpsFunctionPointer;
 
 @end
