ed17e48d18d42cd1459da03eee5201090442c856
rachguo <rachguo@rachguos-Mini.attlocal.net>
2023-12-29 15:34:32 -0800

    add changes for configuring macos support in spm

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ import class Foundation.ProcessInfo
 
 let package = Package(
     name: "onnxruntime",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v12),
+                .macOS(.v11)],
     products: [
         .library(name: "onnxruntime",
                  type: .static,
